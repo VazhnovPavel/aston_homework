3db1e3f251f7c419125e1f96fc959c281e24f2bb
VazhnovPavel <pavel.fortex@yandex.ru>
2024-03-24 13:02:48 +0300

    initial
    initial project

---
+++ src/main/java/ru/vazhnov/aston_homework/AstonHomeworkApplication.java
@@ -0,0 +1,13 @@
+package ru.vazhnov.aston_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstonHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstonHomeworkApplication.class, args);
+    }
+
+}
